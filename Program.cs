680bb75a727cdc60b78efd7d4816cb5815ba2ba4
tatn <tatn@users.noreply.github.com>
2024-06-13 19:49:44 +0900

    A01 - The First Problemの回答を追加。

--- Program.cs
+++ Program.cs
@@ -1,2 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿KyoproTessokuCsharp.chap01.A01.Main(new string[0] { });
---
+++ chap01/A01.cs
@@ -0,0 +1,13 @@
+﻿namespace KyoproTessokuCsharp.chap01
+{
+    internal class A01
+    {
+        public static void Main(string[] args)
+        {
+            int ReadInt() => int.Parse(Console.ReadLine()!);
+
+            int N = ReadInt();
+            Console.WriteLine(N * N);
+        }
+    }
+}
